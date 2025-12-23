f3d742aa5b24b37d9ae6ff710af08edb1233f9a1
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-12-23 10:49:56 +0530

    Fix: otp storage Redis

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,9 @@ dotenv.config();
 const app: Application = express();
 
 app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());        // ✅ REQUIRED
+app.use(express.urlencoded({ extended: true })); // optional
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
